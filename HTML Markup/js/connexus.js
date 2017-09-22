b0762a251ce5360f2dc32195a5e2a9aa8f37d81f
rayolanderos <rayolanderos@gmail.com>
2017-09-21 22:11:25 -0500

    Create and Sorted Tables
    Template: Create
    Vendor: Datatables for sorting in "manage", tagsinput for friendly tags in "create". Form styling.

--- HTML Markup/js/connexus.js
+++ HTML Markup/js/connexus.js
@@ -1,3 +1,17 @@
 $(function() {
+
+    $('#table-streams-i-own').DataTable( {
+        searching: false,
+    	ordering:  true, 
+        paging: false
+    } );
+
+    $('#table-streams-i-subscribe').DataTable( {
+        searching: false,
+    	ordering:  true, 
+        paging: false
+    } );
+
     
 });
+
