11df1f4008ddfb8ef8ff7b0e32145fedffa3da4b
rayolanderos <rayolanderos@gmail.com>
2017-09-26 21:18:06 -0500

    Simplified Login, Manage Service
    Simplified login screen that directs to default google authentication.
    
    Manage service, view, and url activation.
    
    Replaced "Social" for "logout"

---
+++ controllers/manage.py
@@ -0,0 +1,30 @@
+import webapp2
+import jinja2
+import json
+import os
+
+from google.appengine.api import urlfetch
+from google.appengine.api import users
+
+templates_dir = os.path.normpath(os.path.dirname(__file__) + '/../www/')
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(templates_dir),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+
+class Manage(webapp2.RequestHandler):
+
+    def get(self):
+        print(self.uri_for('api-manage'))
+        result = urlfetch.fetch(self.uri_for('api-manage', _full=True))
+
+        if result.status_code == 200:
+            logout_url = users.create_logout_url('/')
+            j = json.loads(result.content)
+            streams_data = {
+            'streams': j, 
+            'logout_url': logout_url
+            }
+            template = JINJA_ENVIRONMENT.get_template('manage.html')
+            self.response.write(template.render(streams_data))
