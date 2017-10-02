02fca5535547c10bbd3b7dd30596fc9e7dfe7d64
gwoodard3 <31335022+gwoodard3@users.noreply.github.com>
2017-10-02 18:13:58 -0500

    Add files via upload

---
+++ controllers/search.py
@@ -0,0 +1,25 @@
+import webapp2
+import jinja2
+import json
+import os
+
+from google.appengine.api import urlfetch
+from google.appengine.api import users
+
+templates_dir = os.path.normpath(os.path.dirname(__file__) + '/../www/')
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(templates_dir),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+
+class Search(webapp2.RequestHandler):
+
+    def get(self):
+        result = urlfetch.fetch(self.uri_for('api-search', _full=True))
+        logout_url = users.create_logout_url('/')
+        if result.status_code == 200:
+            j = json.loads(result.content)
+            streams_data = {'streams': j, 'page_name': 'search', 'logout_url': logout_url}
+            template = JINJA_ENVIRONMENT.get_template('view.html')
+            self.response.write(template.render(streams_data))
