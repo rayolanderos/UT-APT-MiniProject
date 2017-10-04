50d84f7720fee24830ad88e1f800eaca0b039f12
gwoodard3 <31335022+gwoodard3@users.noreply.github.com>
2017-10-04 18:35:25 -0500

    Add files via upload

--- services/search.py
+++ services/search.py
@@ -1,6 +1,8 @@
 import webapp2
 import json
 
+from google.appengine.api import search
+
 from models.stream import Stream
 
 class Search(webapp2.RequestHandler):
@@ -12,11 +14,31 @@ class Search(webapp2.RequestHandler):
         stream_list = []
 
 
-        search = input-search.fetch() # not sure how to grab the contents of the search box
+        # search = input-search.fetch() # not sure how to grab the contents of the search box
 
-        for stream in streams:
-            if search in stream.name or search in stream.tags or search in stream.key.id():
-                stream_list.append({'id': stream.key.id(), 'name': stream.name, 'cover_url': stream.cover_url, 'tags': stream.tags})
+        # for stream in streams:
+        #     if search in stream.name or search in stream.tags or search in stream.key.id():
+        #         stream_list.append({'id': stream.key.id(), 'name': stream.name, 'cover_url': stream.cover_url, 'tags': stream.tags})
+
+        query = input-search.fetch() # not sure how to grab the contents of the search box
+
+        try:
+            index = search.Index('streams', namespace='namespace') # temporary as don't know current namespace if created
+            search_results = index.search(query)
+            returned_count = len(search_results.results)
+            number_found = search_results.number_found
+            for doc in search_results:
+                doc_id = doc.doc_id
+                fields = doc.fields
+        except search.Error:
+            doc_id = None
+            fields = None
+            number_found = 0
+
+        # ...
+
+        for stream_field in fields:
+            stream_list.append(stream_field)
 
 
         print(stream_list)
