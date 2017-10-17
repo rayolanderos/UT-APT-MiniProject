9f0bf2d060a2753218bd42d0a4d946209e48dac0
memo <gleonoliva@users.noreply.github.com>
2017-10-16 22:18:25 -0500

    Added base controller and model

---
+++ androidapp/Connexus/app/src/main/java/com/green/apt/connexus/controllers/BaseController.java
@@ -0,0 +1,12 @@
+package com.green.apt.connexus.controllers;
+
+/**
+ * Created by memo on 16/10/17.
+ * Extend from this class if you create a new Controller.
+ * Controllers manage both the models and the views (user actions)
+ * Push common logic among controllers to this BaseController as you see fit.
+ */
+
+public abstract class BaseController {
+
+}
